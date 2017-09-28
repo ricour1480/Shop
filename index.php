4eca75765f464651f15b39f3f6d653e83f9f36a7
ricour1480 <ricour1480@hotmail.be>
2017-09-28 10:17:20 +0200

    Mise à jour de l'index+css

--- index.php
+++ index.php
@@ -5,23 +5,24 @@ session_start();
     <head>
         <title>Petit application web</title>
         <meta charset="iso-8859-1" />
-    </head>
+        <link rel="stylesheet" type="text/css" href="./lib/css/style_page.css"/>
+     </head>
     <body>
         <header>
-            <h3>Application pour un cinéma </h3>
-            <figure>
+            <h3 id="titre">Application pour un cinéma </h3>
+            <figure id="banniere">
                 <img src="./images/cinema.jpg" alt="cinema"/>
             </figure>
         </header>
-        <section>
-            <nav>
+        <section id="principale">
+            <nav id="menu">
                 <ul>
-                    <li>Filmes &aacute; voir</li>
-                    <li>Reservations</li>
-                    <li>Contact</li>
+                    <li><a href="">Filmes &aacute; voir</a></li>
+                    <li><a href="">Reservation</a></li>
+                    <li><a href="">Contact</a></li>
                 </ul>
             </nav>
-            <section>
+            <section id="page">
                 <?php
                 if (!isset($_SESSION['page'])) {
                     $_SESSION['page'] = "./pages/accueil.php";
@@ -39,11 +40,11 @@ session_start();
                 ?>
             </section>
             <aside>
-                <figure>
+                <figure class="pub">
                     <img src="./images/pub.jpg" alt="pub"/>
                 </figure>
             </aside>
-            <footer>D&eacute;veloppé par Ricour Christopher</footer>
+            <footer id="footer">D&eacute;veloppé par Ricour Christopher</footer>
         </section>
     </body>
 </html>
